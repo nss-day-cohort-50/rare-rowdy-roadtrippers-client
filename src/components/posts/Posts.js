01fa2220d847b666096cbb16da74980d6e16f7ee
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-10-26 15:49:36 -0500

    added posts.js and functionality to render posts list

---
+++ src/components/posts/Posts.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router-dom"
+
+
+export const Posts = () => {
+    const [posts, updatePosts] = useState([])
+    const history = useHistory()
+
+
+    useEffect(
+        () => {
+            return fetch ("http://localhost:8088/posts")
+            .then(response => response.json())
+            .then((posts) => {
+                updatePosts(posts)
+            })
+        },
+        []
+    )
+
+    const deletePost = (id) => {
+        fetch(`http://localhost:8088/posts/${id}`, {
+            method: "DELETE"
+        })
+            .then(() => {
+                return fetch ("http://localhost:8088/posts")
+                .then((posts) => {
+                    updatePosts(posts)
+                })
+            })
+    }
+
+    return (
+        <>
+            <div>
+                <button className="createPost" onClick={() => history.push("/posts/create")}>New Post</button>
+            </div>
+
+            {
+                posts.map(
+                    (post) => {
+                        return <div key={post.id} className="posts__list">
+                            <section>
+                                <h4 key={post.id}>Posted by...</h4>
+                                <div className="item__postList">Post Title: {post.title}</div>
+                                <div className="item__postList">Posted by: {post.user_id}</div>
+                                <div className="item__postList">Content: {post.content}</div>
+                                <div className="item__postList">Category: {post.category}</div>
+                                <div className="item__postList">Publication Date: {post.date}</div>
+                            </section>
+                        </div>
+                    }
+                )
+            }
+        </>
+    )
+}
