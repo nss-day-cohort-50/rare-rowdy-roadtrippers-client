ea1172291be1843432fc646abb44dac8d0860041
Matthew Singler <matthew.singler@gmail.com>
2021-11-16 11:57:07 -0600

    registration module functioning

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -24,11 +24,11 @@ export const NavBar = () => {
                 <Link className="navbar__link" to="/tags">Tag Management</Link>
             </li>
             {
-                (localStorage.getItem("rare_user_id") !== null) ?
+                (localStorage.getItem("token") !== null) ?
                     <li className="nav-item">
                         <button className="nav-link fakeLink"
                             onClick={() => {
-                                localStorage.removeItem("rare_user_id")
+                                localStorage.removeItem("token")
                                 history.push({ pathname: "/" })
                             }}
                         >Logout</button>
